15fd836f17a0b6c3d623f24cebc45d74e9465bdd
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-10-25 10:55:50 +0500

    player and firebase gradle added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-analytics-ktx:21.3.0")
-    implementation("com.google.firebase:firebase-storage:20.3.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -50,8 +49,19 @@ dependencies {
     implementation("com.makeramen:roundedimageview:2.3.0")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
+    implementation("com.google.firebase:firebase-analytics-ktx:21.3.0")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("com.google.firebase:firebase-auth:22.1.2")
     implementation("com.google.firebase:firebase-firestore:24.8.1")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+
+    implementation("com.google.android.exoplayer: exoplayer: 2.10.8")
+    implementation("com.google. android.exoplayer: excplayer-core:2.10.8")
+    implementation("com.google. android.exoplayer: excplayer-dash:2.10.8")
+    implementation("com.google. android.exoplayer: excplayer-hls: 2.10.8")
+    implementation("com.google. android. exoplayer : exoplayer-smoothstreaning:2.10.8")
+    implementation("com.google.android.exoplayer: excplayer-ui: 2.10.8")
+
 
 
 }
