ba31e45ed8bf690bd11d721f7bc5085d6a3b785e
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-10-26 18:28:48 +0500

    Settings_activity.java is successfully changed.

--- app/src/main/java/uz/jasurbekruzimov/smartchild/NavMenu/Settings_activity.java
+++ app/src/main/java/uz/jasurbekruzimov/smartchild/NavMenu/Settings_activity.java
@@ -3,20 +3,52 @@ package uz.jasurbekruzimov.smartchild.NavMenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
-import com.google.firebase.auth.FirebaseAuth;
+import java.util.ArrayList;
+import java.util.List;
 
 import uz.jasurbekruzimov.smartchild.R;
 
 
+//public class
+////    RecyclerView Mrecyclerview;
+////    FirebaseDatabase firebaseDatabase;
+////    DatabaseReference databaseReference;
+//
+//
+//@SuppressLint("MissingInflatedId")
+//@Override
+//protected void onCreate(Bundle savedInstanceState){
+//        super.onCreate(savedInstanceState);
+//
+////
+////        Mrecyclerview = findViewById(R.id.videoList);
+////        Mrecyclerview.setHasFixedSize(true);
+////        Mrecyclerview.setLayoutManager(new LinearLayoutManager(this));
+////   firebaseDatabase = FirebaseDatabase.getInstance();
+////   databaseReference = firebaseDatabase.getReference("video")
+//
+//        }
+//
+////    @Override
+////    protected void onStart() {
+////        super.onStart();
+////
+////        FirebaseRecylerAdaper<UserModel, ViewHolder> firebaseRecylerAdaper = new FirebaseRecy
+////
+////
+////    }
+//        }
+
 public class Settings_activity extends AppCompatActivity {
     LinearLayout backHome;
+    private List<String> videoUrls = new ArrayList<>();
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference storageRef = storage.getReference();
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -25,10 +57,8 @@ public class Settings_activity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
 
-        backHome=findViewById(R.id.backHome);
+        backHome = findViewById(R.id.backHome);
         backHome.setOnClickListener(v -> finish());
-
-
 
 
 
