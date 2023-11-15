0c61fb5c96bfd70dc1a4f24e610ab9c49e73dfca
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-11-15 22:23:49 +0500

    PlayingGame is successfully changed

--- app/src/main/java/uz/jasurbekruzimov/smartchild/ShapesGame/PlayingGame.java
+++ app/src/main/java/uz/jasurbekruzimov/smartchild/ShapesGame/PlayingGame.java
@@ -1,45 +1,42 @@
 package uz.jasurbekruzimov.smartchild.ShapesGame;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
+import uz.jasurbekruzimov.smartchild.Dashboard.Alifbe_Activity;
+import uz.jasurbekruzimov.smartchild.Game.HarfInfoDialog;
 import uz.jasurbekruzimov.smartchild.R;
 import uz.jasurbekruzimov.smartchild.databinding.FragmentPlayingGameBinding;
 
 public class PlayingGame extends Fragment {
+
+    private FragmentPlayingGameBinding binding;
+    private final ArrayList<ImageView> shapes = new ArrayList<>();
+    private final HashMap<String, Integer> shapesMap = new HashMap<>();
 
     public PlayingGame() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    private FragmentPlayingGameBinding binding;
-
-    ArrayList<ImageView> shapes = new ArrayList<>();
-    HashMap<String, Integer> shapesMap = new HashMap<>();
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentPlayingGameBinding.inflate(inflater, container, false);
 
         shapesMap.put("circle", R.drawable.circle);
@@ -49,8 +46,17 @@ public class PlayingGame extends Fragment {
         shapesMap.put("star", R.drawable.star);
         shapesMap.put("pentagon", R.drawable.pentagon);
 
-        binding.btnNext.setOnClickListener(v -> {
-            generateShapes();
+        binding.btnNext.setOnClickListener(v -> generateShapes());
+
+        NavOptions navOptions = new NavOptions.Builder()
+                .setEnterAnim(R.anim.enter)
+                .setExitAnim(R.anim.exit)
+                .setPopEnterAnim(R.anim.pop_enter)
+                .setPopExitAnim(R.anim.pop_exit)
+                .build();
+
+        binding.btnBackGame.setOnClickListener(v -> {
+            Navigation.findNavController(container).navigate(R.id.beginGame, null, navOptions);
         });
 
         shapes.add(binding.ivShape1);
@@ -59,9 +65,7 @@ public class PlayingGame extends Fragment {
         generateShapes();
 
         return binding.getRoot();
-
     }
-
     private void generateShapes() {
         Set<Integer> randomContainer = new HashSet<>();
 
@@ -72,17 +76,18 @@ public class PlayingGame extends Fragment {
             }
             switchMethod(shape, random);
         }
-
         randomContainer.clear();
     }
 
-
+    @SuppressLint("SetTextI18n")
     private void switchMethod(ImageView shape, Integer random) {
         switch (random) {
             case 0:
                 shape.setImageResource(shapesMap.get("circle"));
                 shape.setOnClickListener(v -> {
                     binding.tvAnswer.setText("Aylana");
+                    final MediaPlayer mp = MediaPlayer.create(requireContext(), R.raw.aylana);
+                    mp.start();
                 });
                 break;
 
@@ -90,35 +95,43 @@ public class PlayingGame extends Fragment {
                 shape.setImageResource(shapesMap.get("polygon"));
                 shape.setOnClickListener(v -> {
                     binding.tvAnswer.setText("oltiburchak");
+                    final MediaPlayer mp = MediaPlayer.create(requireContext(), R.raw.oltiburchak);
+                    mp.start();
                 });
                 break;
             case 2:
                 shape.setImageResource(shapesMap.get("triangle"));
                 shape.setOnClickListener(v -> {
                     binding.tvAnswer.setText("uchburchak");
+                    final MediaPlayer mp = MediaPlayer.create(requireContext(), R.raw.uchburchak);
+                    mp.start();
                 });
                 break;
             case 3:
                 shape.setImageResource(shapesMap.get("rectangle"));
                 shape.setOnClickListener(v -> {
                     binding.tvAnswer.setText("to'rtburchak");
+                    final MediaPlayer mp = MediaPlayer.create(requireContext(), R.raw.tortburchak);
+                    mp.start();
                 });
                 break;
             case 4:
                 shape.setImageResource(shapesMap.get("star"));
                 shape.setOnClickListener(v -> {
                     binding.tvAnswer.setText("yulduzcha");
+                    final MediaPlayer mp = MediaPlayer.create(requireContext(), R.raw.yulduzcha);
+                    mp.start();
                 });
                 break;
             case 5:
                 shape.setImageResource(shapesMap.get("pentagon"));
                 shape.setOnClickListener(v -> {
                     binding.tvAnswer.setText("beshburchak");
+                    final MediaPlayer mp = MediaPlayer.create(requireContext(), R.raw.beshburchak);
+                    mp.start();
                 });
         }
     }
-
-    // we had better write these codes for onDestroyView
     @Override
     public void onDestroyView() {
         super.onDestroyView();
