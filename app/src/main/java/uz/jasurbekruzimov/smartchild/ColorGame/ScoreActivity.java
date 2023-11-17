5178db7b3995ee44a4c807a9e76d7274e90ea118
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-11-17 23:54:02 +0500

    Score Activity added and all codes pushed

---
+++ app/src/main/java/uz/jasurbekruzimov/smartchild/ColorGame/ScoreActivity.java
@@ -0,0 +1,16 @@
+package uz.jasurbekruzimov.smartchild.ColorGame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import uz.jasurbekruzimov.smartchild.R;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+}
