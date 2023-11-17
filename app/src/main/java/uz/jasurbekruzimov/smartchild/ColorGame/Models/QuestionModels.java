1c314e97cd552593f9bcbe9b3d6e51daada594f7
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-11-17 23:55:11 +0500

    Set Models and Question Models is added.

---
+++ app/src/main/java/uz/jasurbekruzimov/smartchild/ColorGame/Models/QuestionModels.java
@@ -0,0 +1,66 @@
+package uz.jasurbekruzimov.smartchild.ColorGame.Models;
+
+public class QuestionModels {
+    String question, optionA, optionB, optionC, optionD, correctAnswer;
+
+    public QuestionModels(String question, String optionA, String optionB, String optionC, String optionD, String correctAnswer) {
+        this.question = question;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.optionD = optionD;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public QuestionModels() {
+
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getOptionA() {
+        return optionA;
+    }
+
+    public void setOptionA(String optionA) {
+        this.optionA = optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public void setOptionB(String optionB) {
+        this.optionB = optionB;
+    }
+
+    public String getOptionC() {
+        return optionC;
+    }
+
+    public void setOptionC(String optionC) {
+        this.optionC = optionC;
+    }
+
+    public String getOptionD() {
+        return optionD;
+    }
+
+    public void setOptionD(String optionD) {
+        this.optionD = optionD;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+}
---
+++ app/src/main/java/uz/jasurbekruzimov/smartchild/ColorGame/Models/SetsModels.java
@@ -0,0 +1,18 @@
+package uz.jasurbekruzimov.smartchild.ColorGame.Models;
+
+public class SetsModels {
+    public SetsModels(String setName) {
+        this.setName = setName;
+    }
+
+    public String getSetName() {
+        return setName;
+    }
+
+    public void setSetName(String setName) {
+        this.setName = setName;
+    }
+
+    String setName;
+
+}
