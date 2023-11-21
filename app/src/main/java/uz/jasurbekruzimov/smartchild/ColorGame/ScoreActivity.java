5939613a0826c70bcf50eb42aabfcaca0d754d0b
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-11-21 22:12:18 +0500

    ScoreActivity.java is added and all codes successfully pushed

--- app/src/main/java/uz/jasurbekruzimov/smartchild/ColorGame/ScoreActivity.java
+++ app/src/main/java/uz/jasurbekruzimov/smartchild/ColorGame/ScoreActivity.java
@@ -2,15 +2,50 @@ package uz.jasurbekruzimov.smartchild.ColorGame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.color.utilities.Score;
 
 import uz.jasurbekruzimov.smartchild.R;
+import uz.jasurbekruzimov.smartchild.databinding.ActivityScoreBinding;
 
 public class ScoreActivity extends AppCompatActivity {
 
+    ActivityScoreBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityScoreBinding.inflate(getLayoutInflater()) ;
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_score);
+        setContentView(binding.getRoot());
+
+
+
+        int totalScore = getIntent().getIntExtra("Umumiy", 0);
+        int correctAnsw = getIntent().getIntExtra("Natija", 0);
+
+        int wrong = totalScore-correctAnsw;
+
+        binding.totalQuestions.setText(String.valueOf(totalScore));
+        binding.rightAnsw.setText(String.valueOf(correctAnsw));
+        binding.wrongAnsw.setText(String.valueOf(wrong));
+
+        binding.btnRetry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScoreActivity.this, SetsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        binding.btnQuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
     }
 }
