9acfd65015ba59d8639240ed66a2cca1863ebf6c
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-11-24 00:20:14 +0500

    Settings_activity.java class and xml file is update.

--- app/src/main/java/uz/jasurbekruzimov/smartchild/NavMenu/Settings_activity.java
+++ app/src/main/java/uz/jasurbekruzimov/smartchild/NavMenu/Settings_activity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -13,39 +14,8 @@ import java.util.List;
 
 import uz.jasurbekruzimov.smartchild.R;
 
-
-//public class
-////    RecyclerView Mrecyclerview;
-////    FirebaseDatabase firebaseDatabase;
-////    DatabaseReference databaseReference;
-//
-//
-//@SuppressLint("MissingInflatedId")
-//@Override
-//protected void onCreate(Bundle savedInstanceState){
-//        super.onCreate(savedInstanceState);
-//
-////
-////        Mrecyclerview = findViewById(R.id.videoList);
-////        Mrecyclerview.setHasFixedSize(true);
-////        Mrecyclerview.setLayoutManager(new LinearLayoutManager(this));
-////   firebaseDatabase = FirebaseDatabase.getInstance();
-////   databaseReference = firebaseDatabase.getReference("video")
-//
-//        }
-//
-////    @Override
-////    protected void onStart() {
-////        super.onStart();
-////
-////        FirebaseRecylerAdaper<UserModel, ViewHolder> firebaseRecylerAdaper = new FirebaseRecy
-////
-////
-////    }
-//        }
-
 public class Settings_activity extends AppCompatActivity {
-    LinearLayout backHome;
+    ImageView backHome;
     private List<String> videoUrls = new ArrayList<>();
     private FirebaseStorage storage = FirebaseStorage.getInstance();
     private StorageReference storageRef = storage.getReference();
@@ -56,11 +26,8 @@ public class Settings_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-
         backHome = findViewById(R.id.backHome);
         backHome.setOnClickListener(v -> finish());
-
-
 
     }
 
