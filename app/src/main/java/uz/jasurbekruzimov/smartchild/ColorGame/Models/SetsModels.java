c3c3c77cc618eed46e16d12fcf968ea3a1fa9446
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-11-24 00:21:52 +0500

    Sets and Question Models is update.

--- app/src/main/java/uz/jasurbekruzimov/smartchild/ColorGame/Models/SetsModels.java
+++ app/src/main/java/uz/jasurbekruzimov/smartchild/ColorGame/Models/SetsModels.java
@@ -4,15 +4,12 @@ public class SetsModels {
     public SetsModels(String setName) {
         this.setName = setName;
     }
-
     public String getSetName() {
         return setName;
     }
-
     public void setSetName(String setName) {
         this.setName = setName;
     }
-
     String setName;
 
 }
