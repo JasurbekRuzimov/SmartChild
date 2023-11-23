1520314ffac1abb18435a0d59fe13ea092072c07
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-11-24 00:18:51 +0500

    oyinlar class and xml file is update.

--- app/src/main/java/uz/jasurbekruzimov/smartchild/Game/Oyinlar_Activity.java
+++ app/src/main/java/uz/jasurbekruzimov/smartchild/Game/Oyinlar_Activity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import uz.jasurbekruzimov.smartchild.R;
@@ -13,7 +14,7 @@ import uz.jasurbekruzimov.smartchild.R;
 
 public class Oyinlar_Activity extends AppCompatActivity {
 
-    LinearLayout backHome;
+    ImageView backHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
