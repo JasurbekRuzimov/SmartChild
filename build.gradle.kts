dda2c331fd891b9ad052286d91df57d679d09458
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-04-24 08:07:47 +0500

    gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
-        classpath("com.android.tools.build:gradle:8.1.3")
+        classpath("com.android.tools.build:gradle:8.2.2")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.3" apply false
+    id("com.android.application") version "8.2.2" apply false
 }
